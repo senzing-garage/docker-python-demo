a3adc3afde695f2a150350d607224dd754eca1ef
docktermj <michael@dockter.com>
2022-09-29 14:29:04 -0400

    #88 Prepare for versioned release

--- rootfs/app/app.py
+++ rootfs/app/app.py
@@ -36,9 +36,9 @@ APP = Flask(__name__)
 # Metadata
 
 __all__ = []
-__version__ = "1.5.0"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.5.1"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2018-10-29'
-__updated__ = '2022-09-07'
+__updated__ = '2022-09-29'
 
 # -----------------------------------------------------------------------------
 # Senzing configuration.
